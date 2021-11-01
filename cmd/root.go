e006e7aeda97947905a19dec7ddbe0dfad78b34a
thinhvu2703 <phuocthinhvu@gmail.com>
2021-11-01 10:16:14 +0700

    update-1

--- cmd/root.go
+++ cmd/root.go
@@ -31,6 +31,9 @@ var rootCmd = &cobra.Command{
 	Use:   "golang-CLI-app",
 	Short: "CLI APP",
 	Long:  `A test CLI APP`,
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -49,11 +52,9 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.golang-CLI-app.yaml)")
-
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
 }
 
 // initConfig reads in config file and ENV variables if set.
